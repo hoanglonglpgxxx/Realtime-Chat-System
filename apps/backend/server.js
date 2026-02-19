52440d09f4f7ce00ea1d468cacec610bb1b7fa11
Mít <hoanglonglpgxxx@gmail.com>
2026-02-19 19:46:35 +0700

    fix fetching users data errors

--- apps/backend/server.js
+++ apps/backend/server.js
@@ -58,6 +58,6 @@ mongoose.connection.once('open', async () => {
 });
 
 app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/user', userRouter);
+app.use('/api/v1/users', userRouter);
 app.use('/api/v1/rooms', roomRouter);
 app.use('/api/v1/messages', messageRouter);
